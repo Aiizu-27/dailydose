fbddd71773241b626bd4636f7fc762f0f11c9c8e
Aiizu-27 <luciiiia.2713@gmail.com>
2026-05-20 19:46:06 +0200

    borrar datos

--- BaseDatos/insercion_promociones.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO PROMOCIONES (NOMBRE, DESCRIPCION, PUNTOS_REQUERIDOS) VALUES
-('Envío a Domicilio Gratis', 'Haz tu pedido online y te lo llevamos a casa sin ningún gasto de envío.', 40),
-('Cold Brew Refrescante', 'Combate el calor con nuestro espectacular Cold Brew macerado durante 24h.', 70),
-('Desayuno Perfecto', 'Añade un croissant de mantequilla recién horneado a tu pedido de café.', 80),
-('Combo Burger Completo', 'Convierte tu hamburguesa en menú con ración de patatas caseras y bebida.', 150),
-('Descuento de 5€', 'Descuento directo de 5 euros aplicable al total de cualquier pedido.', 200),
-('Paquete de Café (250g)', 'Llévate a casa un paquete de nuestro grano de especialidad en cartel.', 400),
-('Taza Oficial DailyDose', 'Lleva el estilo a casa. Taza de cerámica exclusiva con nuestro logo.', 500);
