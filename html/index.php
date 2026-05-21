e8ed1acc12e5cbf2e5a87a2a4d804aa4fed38489
aiizu-27 <luciiacampos.27@gmail.com>
2026-05-21 21:52:19 +0200

    favicom

--- html/index.php
+++ html/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DAILY DOSE - Tu dosis diaria de café</title>
+    <link rel="icon" type="image/png" href="assets/img/APP.png">
 
     <!-- CSS Global -->
     <link rel="stylesheet" href="assets/css/variables.css">
