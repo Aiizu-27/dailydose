1a3dc2424bf2156354494f6f4a219d6bf180305f
aiizu-27 <luciiacampos.27@gmail.com>
2026-05-24 21:56:03 +0200

    retoques esteticos

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,28 +1,37 @@
 <footer class="footer-cristal">
     <div class="footer-contenido">
-      <div class="footer-info">
-        <h3>DAILY DOSE</h3>
-        <p>Tu dosis diaria de café.</p>
-        <p>Calle del Café, 12 · Madrid</p>
-        <p><a href="mailto:contacto@daily-dose.es">contacto@daily-dose.es</a></p>
-      </div>
-
-      <div class="iconos-redes">
-          <a href="#" aria-label="Síguenos en Instagram" class="icono-red">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z"/>
-            </svg>
-          </a>
-
-          <a href="#" aria-label="Síguenos en Twitter" class="icono-red">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10.03 2.38,10.06 2.38,10.1C2.38,12.18 3.86,13.92 5.83,14.31C5.47,14.41 5.09,14.46 4.7,14.46C4.42,14.46 4.15,14.41 3.88,14.36C4.43,16.06 6.02,17.3 7.89,17.34C6.44,18.46 4.6,19.13 2.61,19.13C2.26,19.13 1.92,19.11 1.58,19.07C3.44,20.29 5.66,21 8.05,21C15.82,21 20.07,14.56 20.07,9C20.07,8.81 20.06,8.63 20.05,8.45C20.87,7.85 21.6,7.11 22.19,6.27C21.43,6.6 20.62,6.83 19.77,6.93C20.64,6.41 21.3,5.59 21.6,4.59L22.46,6Z"/>
-            </svg>
-          </a>
+        
+        <div class="footer-info">
+            <h3>DAILY DOSE</h3>
+            <p>Tu dosis diaria de café.</p>
+            <p>Calle del Café, 12 · Madrid</p>
+            <p><a href="mailto:contacto@daily-dose.es">contacto@daily-dose.es</a></p>
         </div>
+
+        <div class="iconos-redes">
+            <a href="#" aria-label="Síguenos en Instagram" class="icono-red">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z"/>
+                </svg>
+            </a>
+
+            <a href="#" aria-label="Síguenos en Twitter" class="icono-red">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10.03 2.38,10.06 2.38,10.1C2.38,12.18 3.86,13.92 5.83,14.31C5.47,14.41 5.09,14.46 4.7,14.46C4.42,14.46 4.15,14.41 3.88,14.36C4.43,16.06 6.02,17.3 7.89,17.34C6.44,18.46 4.6,19.13 2.61,19.13C2.26,19.13 1.92,19.11 1.58,19.07C3.44,20.29 5.66,21 8.05,21C15.82,21 20.07,14.56 20.07,9C20.07,8.81 20.06,8.63 20.05,8.45C20.87,7.85 21.6,7.11 22.19,6.27C21.43,6.6 20.62,6.83 19.77,6.93C20.64,6.41 21.3,5.59 21.6,4.59L22.46,6Z"/>
+                </svg>
+            </a>
+        </div>
+
+        <div class="footer-digitalizador">
+            <span class="texto-digitalizador">Solución por:</span>
+            <a href="https://cds-advance.es" target="_blank" class="enlace-logo-cds">
+                <img src="../assets/img/logo_CDS.jpg" alt="Logo Empresa Digitalizadora" class="logo-cds">
+            </a>
+        </div>
+
     </div>
 
     <div class="footer-copy">
-      <p>&copy; 2025 DAILY DOSE · Todos los derechos reservados</p>
+        <p>&copy; 2026 DAILY DOSE · Todos los derechos reservados</p>
     </div>
 </footer>
