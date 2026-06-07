80ce9816055c7661638db6843afb78565dc43519
aiizu-27 <luciiacampos.27@gmail.com>
2026-06-07 22:29:50 +0200

    arreglo de colores

--- html/carta.php
+++ html/carta.php
@@ -49,7 +49,7 @@ $conn->close();
     <link rel="stylesheet" href="assets/css/style.css?v=2">
 
     <link rel="stylesheet" href="assets/css/header.css?v=2">
-    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/footer.css?v=2">
     <link rel="stylesheet" href="assets/css/carta.css?v=4">
 </head>
 <body>
